ef5ae2e866f6811b03d0133ad56b561b657d7c96
esadeli <efrat.sadeli@gmail.com>
2018-09-13 11:13:17 +0700

    All scenario Server OK

--- server/controllers/QuoteController.js
+++ server/controllers/QuoteController.js
@@ -15,17 +15,43 @@ class QuoteController {
             .catch(error =>{
                 res.status(500).json({ msg : error})
             })
-
     }
 
     // list of all qoute
     static listQuote(req,res){
+
+        Quote.find({})
+            .then(quotes =>{
+                res.status(201).json(quotes);
+            })
+            .catch(error =>{
+                res.status(500).json({ msg : 'ERROR ',error});
+            })
 
     }
 
     // delete quote
     static deleteQuote(req,res){
 
+        // check user id :
+        // console.log('Aktif USer',req.decoded)
+    
+        Quote.findOneAndRemove({ _id : req.params.id, user : req.decoded.id})
+            .then(quote =>{
+                console.log('DELETE-->',quote)
+
+                if(quote){
+                    res.status(201).json({ 
+                        "success": true,
+                        "message": `Quote with id ${quote._id} deleted`
+                    })
+                }else{
+                    res.status(403).json({ msg : 'You are not authorized to delete this quote' })
+                }
+            })
+            .catch(error =>{
+                res.status(500).json({ msg : 'ERROR ',error});
+            })
     }
 }
 
--- server/routes/QuoteRoutes.js
+++ server/routes/QuoteRoutes.js
@@ -5,12 +5,20 @@ const router = express.Router();
 const IsLogin = require('../middlewares/IsLogin');
 const QuoteController = require('../controllers/QuoteController');
 
-router.use(IsLogin)
 
 // create quote
-router.post('/',(req,res)=>{
+router.post('/',IsLogin,(req,res)=>{
     QuoteController.addQuote(req,res)
 })
 
+// list all quote
+router.get('/',(req,res)=>{
+    QuoteController.listQuote(req,res)
+})
+
+// delete quote
+router.delete('/:id',IsLogin,(req,res)=>{
+    QuoteController.deleteQuote(req,res);
+})
 
 module.exports = router
